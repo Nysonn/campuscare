27509708028939a1a9188c56b6fe90de04905f76
Nysonn <nysonagumya@gmail.com>
2026-03-05 13:13:59 +0300

    added a crisis word

--- internal/chatbot/moderation.go
+++ internal/chatbot/moderation.go
@@ -4,10 +4,23 @@ import "strings"
 
 var crisisKeywords = []string{
 	"suicide",
+	"suicidal",
 	"kill myself",
+	"killing myself",
+	"want to die",
+	"want to kill",
 	"end my life",
+	"ending my life",
+	"take my life",
+	"taking my life",
+	"don't want to live",
+	"don't want to be alive",
+	"no reason to live",
 	"self harm",
-	"cutting",
+	"self-harm",
+	"cutting myself",
+	"hurt myself",
+	"hurting myself",
 	"overdose",
 }
 
