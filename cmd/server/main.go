75e0ca694439aa72c75e11ce06108e7b7846ccf8
Nysonn <nysonagumya@gmail.com>
2026-03-05 19:36:43 +0300

    prod url in cors

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -30,7 +30,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://campuscareug.web.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
