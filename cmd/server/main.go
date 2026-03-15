8847ba93f654d065cd01d3cc1f91f14600554325
Nysonn <nysonagumya@gmail.com>
2026-03-15 12:43:20 +0300

    patch missing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -31,7 +31,7 @@ func main() {
 
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173", "https://campuscareug.web.app", "http://192.168.11.23:5173", "http://172.23.0.1:5173", "http://172.19.0.1:5173"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
 	}))
